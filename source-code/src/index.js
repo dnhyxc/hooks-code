2cd6281ff8a29222a066d739d63520bbaac2f1f9
dengninghai <2640559435@qq.com>
2021-01-10 20:18:18 +0800

    add

--- source-code/src/index.js
+++ source-code/src/index.js
@@ -4,8 +4,8 @@ import ReactDOM from 'react-dom';
 const element = (
   <div id='A1'>
     <div id='B1'>
-      <div id='C1'></div>
-      <div id='C2'></div>
+      <div id='C1'>C1</div>
+      <div id='C2'>C2</div>
     </div>
     <div id='B2'>B2</div>
   </div>
